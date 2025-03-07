265d86b914f51907932941ac8e942762100d9811
Nikhil rai <rainikhil044@gmail.com>
2025-03-07 15:08:15 +0530

    fix cors issue

--- api/server.ts
+++ api/server.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser())
 app.use(express.json());
 app.use( cors({
-  origin: "http://localhost:5173", 
+  origin: "https://jobboardsss.netlify.app'", 
   credentials: true, 
 }))
 app.use('/auth', authRoutes);
